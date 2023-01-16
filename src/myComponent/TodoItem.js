d8f4a5205118dd3930559172ef6fbbebeb0aef02
MayakaOsinde <mayakaosinde@gmail.com>
2023-01-16 10:06:07 +0300

    Commit

--- src/myComponent/TodoItem.js
+++ src/myComponent/TodoItem.js
@@ -1,4 +1,19 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+
+function ToDoItem (){
+
+  const []
+
+  return(
+    <div>
+      <h1>learn react</h1>
+      <p>loreum opsium</p>
+    </div>
+  )
+}
+
+
+
 
 class MyComponent extends Component {
   render() {
